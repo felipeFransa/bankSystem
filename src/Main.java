1ac4207bb0bfc638fa6b8c371cdf9032de685f0a
Felipe <antoniofelipesilva@live.com>
2024-03-26 17:43:38 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Reading = new Scanner(System.in);
+        String firstName = "Felipe Fransa";
+        int age = 27;
+        String password = "a8f3s3f3";
+        String typeAccount = "Corrente";
+        double accountBalance = 3.200;
+
+        System.out.println("************");
+        System.out.println("\nName client: " + firstName);
+        System.out.println("Type Account: " + typeAccount);
+        System.out.println("Balance: " + accountBalance);
+        System.out.println("\n************");
+
+    }
+}
