03aa1f43a9220039031ae2e5c2520060bcc954e4
Felipe <antoniofelipesilva@live.com>
2024-03-26 18:31:18 -0300

    project basic bank system

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         String password = "a8f3s3f3";
         String typeAccount = "Corrente";
         double accountBalance = 3.200;
+        int option = 0;
 
         System.out.println("************");
         System.out.println("\nName client: " + firstName);
@@ -15,5 +16,37 @@ public class Main {
         System.out.println("Balance: " + accountBalance);
         System.out.println("\n************");
 
+        String menu = """
+                ** Digite sua opcão **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor\s
+                4 - Sair
+                """;
+
+        while (option != 4) {
+            System.out.println(menu);
+            option = Reading.nextInt();
+
+            if (option == 1) {
+                System.out.println("Balance: " +accountBalance);
+            } else if (option == 2) {
+                System.out.println("What amount do you want to transfer?");
+                double value = Reading.nextDouble();
+                if (value > accountBalance) {
+                    System.out.println("Not on balance");
+                }else {
+                    accountBalance =- value;
+                    System.out.println("New balance now: " + accountBalance);
+                }
+            } else if (option == 3) {
+                System.out.println("enter the transfer amount: ");
+                double value = Reading.nextDouble();
+                accountBalance += value;
+                System.out.println("Your current balance is: " +accountBalance);
+            } else if (option != 4){
+                System.out.println("Option invalidate.");
+            }
+        }
     }
 }
