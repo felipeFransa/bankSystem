0bf483babdc1749f9c8184149c865a0bfdf124a9
Felipe <antoniofelipesilva@live.com>
2024-03-27 17:49:02 -0300

    add newUser

---
+++ src/newUser.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class newUser {
+        public static void main(String[] args) {
+
+            Scanner Reading = new Scanner(System.in);
+
+            String firstName = "Felipe";
+            int age = 27;
+            double Balance = 3.200;
+            String typeAccount = "Corrente";
+
+            String password = "a8f3s3f3";
+            int bankAccountNumber = 5071;
+
+            boolean accessOrBank = false;
+
+            System.out.println("enter your name:");
+            String nowName = Reading.nextLine();
+            System.out.println("enter your password");
+            String nowPassword = Reading.nextLine();
+            System.out.println("enter your account:");
+            int verificationBankAccountNumber = Reading.nextInt();
+
+            if (bankAccountNumber == verificationBankAccountNumber && nowPassword.equals(password) ) {
+                System.out.println("Account Validate");
+                accessOrBank = true;
+            }else {
+                System.out.println("Account invalidate");
+            }
+            if (accessOrBank){
+                System.out.println("Welcome " +firstName);
+            }
+        }
+}
+
+
