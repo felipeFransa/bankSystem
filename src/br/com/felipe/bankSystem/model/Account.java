49a8ced40b271a72f5146386bf39d77ec562c36c
Felipe Fransa <antoniofelipesilva@live.com>
2024-08-11 19:37:07 -0300

    class account

---
+++ src/br/com/felipe/bankSystem/model/Account.java
@@ -0,0 +1,22 @@
+package br.com.felipe.bankSystem.model;
+
+public class Account {
+    private int numberAccount;
+    private int accountAG;
+
+    public int getNumberAccount() {
+        return numberAccount;
+    }
+
+    public void setNumberAccount(int numberAccount) {
+        this.numberAccount = numberAccount;
+    }
+
+    public int getAccountAG() {
+        return accountAG;
+    }
+
+    public void setAccountAG(int accountAG) {
+        this.accountAG = accountAG;
+    }
+}
