9ec76047f4bf7299bad7d7cfd9a3de312b134095
Felipe Fransa <antoniofelipesilva@live.com>
2024-09-05 18:31:41 -0300

    add new structure

--- src/br/com/felipe/bankSystem/main/MainUser.java
+++ src/br/com/felipe/bankSystem/main/MainUser.java
@@ -2,15 +2,43 @@ package br.com.felipe.bankSystem.main;
 
 import br.com.felipe.bankSystem.model.User;
 
+import java.util.Scanner;
+
 public class MainUser {
     public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
+        Scanner readingNumber = new Scanner(System.in);
         User user = new User();
 
-        user.setName("Felipe");
-        user.setAge(27);
-        user.setPassword("a8f3s3f3");
-        user.setNumberAccount(5071);
-        user.setTypeAccount("corrente");
-        user.setValueAccount(4200);
+        System.out.println("enter your name:");
+        String name = reading.nextLine();
+        user.setName(name);
+
+        System.out.println("enter your age:");
+        Integer age = readingNumber.nextInt();
+        user.setAge(age);
+
+        System.out.println("enter your password:");
+        String password = reading.nextLine();
+        user.setPassword(password);
+
+        System.out.println("enter your AG number account");
+        Integer numberAccount = readingNumber.nextInt();
+        user.setNumberAccount(numberAccount);
+
+        System.out.println("enter your type account");
+        String typeAccount = reading.nextLine();
+        user.setTypeAccount(typeAccount);
+
+        System.out.println("enter your value account:");
+        Integer valueAccount = readingNumber.nextInt();
+        user.setValueAccount(valueAccount);
+
+        System.out.println(user.getName());
+        System.out.println(user.getPassword());
+        System.out.println(user.getNumberAccount());
+        System.out.println(user.getValueAccount());
+        System.out.println(user.getTypeAccount());
+        System.out.println(user.getAge());
     }
 }
